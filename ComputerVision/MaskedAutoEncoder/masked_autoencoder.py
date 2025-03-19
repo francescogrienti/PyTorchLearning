0c348857886b5d55452e61210458375a74280b7b
Francesco Grienti <francesco.grienti@studenti.unimi.it>
2025-03-19 10:35:27 +0100

    test on learning rate

--- ComputerVision/MaskedAutoEncoder/masked_autoencoder.py
+++ ComputerVision/MaskedAutoEncoder/masked_autoencoder.py
@@ -27,7 +27,7 @@ hyper_space = {
     "forward_expansion": 108,
     "patch_size": 4,
     "dropout_rate": 0.1,
-    "learning_rate": 0.001,
+    "learning_rate": 0.0001,
     "num_classes": 10,
     "num_channels": 3,
     "qkv_bias": True,
