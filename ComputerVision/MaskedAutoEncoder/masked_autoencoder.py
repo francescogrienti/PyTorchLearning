9fdbefcb3823339d2e1634339a9e0458aaf01fe7
Francesco Grienti <francesco.grienti@studenti.unimi.it>
2025-04-08 21:09:45 +0200

    Little fix

--- ComputerVision/MaskedAutoEncoder/masked_autoencoder.py
+++ ComputerVision/MaskedAutoEncoder/masked_autoencoder.py
@@ -375,6 +375,20 @@ class MaskedAutoEncoder(nn.Module):
         x = x.reshape(shape=(imgs.shape[0], h * w, self.patch_size ** 2 * 3))
         return x
 
+    def unpatchify(self, x):
+        """
+        x: (N, L, patch_size**2 *3)
+        imgs: (N, 3, H, W)
+        """
+        p = self.patch_embed.patch_size[0]
+        h = w = int(x.shape[1] ** .5)
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, 3))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], 3, h * p, h * p))
+        return imgs
+
     # Function for evaluating the loss between original images and the prediction from the MAE
     def forward_loss(self, imgs, pred, mask):
         """
