34a6e51fb36dd0d5beec15a13c118033288237e1
Francesco Grienti <francesco.grienti.stud@login2ng.indaco.unimi.it>
2025-05-03 21:54:39 +0200

    Adding .sh scripts

---
+++ vit_pretrained.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --partition=math
+#SBATCH --nodes=1
+#SBATCH -c 64  #Numero di CPU per nodo
+#SBATCH --time=120:00:00
+#SBATCH --mem=300GB
+#SBATCH --job-name ViT_pretrained
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+#SBATCH --account=mlnucleation
+#SBATCH --gres=gpu:2  #N Gpus to use
+
+source /exa/software/Spack-2023/spack/share/spack/setup-env.sh
+spack load cuda@12.5.0 arch=linux-rocky8-icelake
+
+EXP=$1
+mkdir -p /home/users/francesco.grienti.stud/MLNucleation/GRIENTI/PyTorchLearning/ComputerVision/MaskedAutoEncoder/plots/vit-pretrained/$EXP
+
+cd /home/users/francesco.grienti.stud/MLNucleation/GRIENTI/PyTorchLearning/ComputerVision/MaskedAutoEncoder/trainer
+echo "Started ViT-pretrained evaluation..." 
+python3 train_full.py --exp_name "$EXP"
+echo "Training done!!"
+
