c6926214bfd68d9740e0592a571c082ff7033286
Francesco Grienti <francesco.grienti@studenti.unimi.it>
2025-08-25 11:05:32 +0200

    Fix font

--- ComputerVision/MaskedAutoEncoder/trainer/model.py
+++ ComputerVision/MaskedAutoEncoder/trainer/model.py
@@ -147,10 +147,10 @@ class MAE_ViT(torch.nn.Module):
     def __init__(self,
                  image_size=32,
                  patch_size=2,
-                 emb_dim=24,
-                 encoder_layer=6,
+                 emb_dim=192,
+                 encoder_layer=12,
                  encoder_head=3,
-                 decoder_layer=3,
+                 decoder_layer=4,
                  decoder_head=3,
                  mask_ratio=0.75,
                  ) -> None:
